0caeb3c32b3a543240bb8cecac24d53176412e1b
davemarch <davemarchaudio@gmail.com>
2019-02-13 13:21:06 +0000

    Score Working

--- script.js
+++ script.js
@@ -3,26 +3,24 @@ const currentDice = document.getElementById('button');
 const input = document.getElementById('input');
 const hideDice = document.getElementById('diceimg');
 const scoredisplay = document.getElementById('scoredisplay');
+let totalScore = 0;
+
 
 diceimg.style.visibility = "hidden";
 scoredisplay.style.visibility = "hidden";
 
-
 currentDice.addEventListener('click', () => {
  getDiceRoll();
+ diceimg.style.visibility = "visible";
+ scoredisplay.style.visibility = "visible";
 })
 
 const getDiceRoll = () => {
   let randomNumber = (Math.floor(Math.random() * 6)+ 1);
-  diceimg.style.visibility = "visible";
-  scoredisplay.style.visibility = "visible";
-  scoredisplay.innerHTML='Your Score is' + ' ' + randomNumber;
+  totalScore = totalScore + randomNumber;
+  scoredisplay.innerHTML='Your Score is' + ' ' + totalScore;
   document.getElementById('diceimg').src=`img/dice${randomNumber}.png`;
 }
-
-
-
-
 
 
 
