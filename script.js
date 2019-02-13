cba8bb92270bd873f1399dbcc6d9244790c733d1
davemarch <davemarchaudio@gmail.com>
2019-02-13 10:40:53 +0000

    .

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const heading = document.getElementById('heading');
 const currentDice = document.getElementById('button');
 const input = document.getElementById('input');
-// const myArray = ["one","two","three","four","five","six"];
 const hideDice = document.getElementById('diceimg');
 const scoredisplay = document.getElementById('scoredisplay');
 
-// let getDiceRoll = myArray[randomNumber];
-// for ( i=0; i<myArray.length; i++) {
-// if (getDiceRoll == myArray[0]){
-//     console.log('you got a ' + getDiceRoll + ' you lose')
-// } else { console.log('you got a ' + getDiceRoll)}
-
+diceimg.style.display = "none";
 
 currentDice.addEventListener('click', () => {
  getDiceRoll();
@@ -18,6 +12,7 @@ currentDice.addEventListener('click', () => {
 
 const getDiceRoll = () => {
   let randomNumber = (Math.floor(Math.random() * 6)+ 1);
+  diceimg.style.display = "show";
   scoredisplay.innerHTML=randomNumber;
   document.getElementById('diceimg').src=`img/dice${randomNumber}.png`;
 }
